d9b61282dbed7db8db153f2ab2b56e3a0b2593d1
yixuan.wang <yixuan.wang@xiaoyanggroup.cn>
2024-05-22 10:52:59 +0800

    init project

--- core/config.py
+++ core/config.py
@@ -1,25 +1,24 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import (
-    AnyUrl,
-    BeforeValidator,
-    HttpUrl,
     PostgresDsn,
     computed_field,
-    model_validator,
-    MySQLDsn
 )
 
 from pydantic_core import MultiHostUrl
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
-    POSTGRES_SERVER: str = "150.158.11.204"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "difyai123456"
-    POSTGRES_DB: str = "dify"
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     @computed_field  # type: ignore[misc]
     @property
